f4c944e12967f2e4abac2ec474848f6d41375151
VANESSAYY <yeyoung1217@naver.com>
2019-06-24 07:10:49 +0900

    first commit

--- myapp/views.py
+++ myapp/views.py
@@ -14,13 +14,9 @@ def detail(request, blog_id):
 
 def comment_new(request, blog_id):
     post = get_object_or_404(Blog, pk=blog_id)
-    if request.method == "POST":
-        form = CommentForm(request.POST)
-        if form.is_valid():
-            comment = form.save(commit=False)
-            comment.post = Blog.objects.get(pk=blog_id)
-            comment.save()
-            return redirect('detail', blog_id)
-    else:
-        form = CommentForm()
-    return render(request, 'blog_form.html', {'form': form,})
+    comment = Comment()
+    comment.post = post
+    comment.author = request.POST['author']
+    comment.text = request.POST['body']
+    comment.save()
+    return render(request, 'detail.html', {'blog': post})
